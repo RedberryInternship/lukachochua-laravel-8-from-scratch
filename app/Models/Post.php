5f1ec246f27ca972111f41c883054bb62b417b78
luka chochua <lukachochua@gmail.com>
2021-10-11 18:02:28 +0400

    feat: switch to collections

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,22 +4,53 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post 
 {
-    public static function all() {
-        $files = File::files(resource_path("posts/"));
-        array_map(function($file) {
-            return $file->getContents();
-        }, $files);
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this -> title = $title;
+        $this -> excerpt = $excerpt;
+        $this -> date = $date;
+        $this -> body = $body;
+        $this -> slug = $slug;
     }
+
+
+    public static function all() {
+        return cache()-> rememberForever('posts.all', function() {
+            return collect(File::files(resource_path("posts")))
+                ->map(function ($file) {
+                    return YamlFrontMatter::parseFile($file);
+                })
+                
+                ->map(function ($document) {
+                return new Post (
+                    $document -> title,
+                    $document -> excerpt,
+                    $document -> date,
+                    $document -> body(),
+                    $document -> slug
+                );
+            }) 
+            ->sortByDesc('date');
+        });        
+    }
+
     public static function find($slug) {
-        if(! file_exists($path = resource_path("/posts/{$slug}.html"))) {
-            Throw new ModelNotFoundException(); 
-        };
-    
-        return cache()->remember("posts.{$slug}", 1200, function() use($path) {
-            return file_get_contents($path);
-        });
+       $posts = static::all();
+       
+       return $posts -> firstWhere("slug", $slug);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +17,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     $posts = Post::all();
+
+
     return view('posts', [
         'posts' => $posts
-    ]);
+    ]); 
 });
 
 
