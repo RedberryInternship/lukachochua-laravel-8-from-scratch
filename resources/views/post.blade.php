7df255b0a25260e84200239e3d482c6a3233cc61
luka chochua <lukachochua@gmail.com>
2021-10-15 20:48:22 +0400

    feat: show all posts associated with category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
     <article>
-        <h1>{{ $post-> title }}</h1>
+        <h1>{{ $post->title }}</h1>
+
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
 
         <div>
-            {!! $post-> body !!}
+            {!! $post->body !!}
         </div>
     </article>
     <a href="/">Go Back</a>
