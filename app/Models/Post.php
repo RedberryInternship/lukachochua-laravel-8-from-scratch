dc403b2e71e9bd29247b480bab6bf86fa2878346
luka chochua <lukachochua@gmail.com>
2021-10-18 20:57:09 +0400

    Feat: start using factories, view all posts by an author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,9 +10,15 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
+    
+    protected $with = ['category', 'author'];
 
     public function category() 
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -18,12 +19,9 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-
-    $posts = Post::all();
-
-
+    
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::latest()->get()
     ]); 
 });
 
@@ -41,3 +39,10 @@ Route::get('categories/{category:slug}', function (Category $category){
         'posts' => $category->posts
     ]); 
 });
+
+Route::get('authors/{author:username}', function (User $author){ 
+    return view('posts', [
+        'posts' => $author->posts
+    ]); 
+});
+
