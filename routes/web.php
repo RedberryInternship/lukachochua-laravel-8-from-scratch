08d7aa358586f83425b1817d2856b266bb01225d
luka chochua <lukachochua@gmail.com>
2021-10-26 17:13:52 +0400

    feat(comments): design and activate comments form
    create a comment form that allows blog user to comment on the posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
@@ -22,16 +23,22 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-//Route::get('login', 'App\Http\Controllers\SessionsController@create')->middleware('guest');
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('sessions', [SessionsController::class, 'store'])->middleware('guest');
 
-Route::post('sessions', 'App\Http\Controllers\SessionsController@store')->middleware('guest');
-Route::post('logout', 'App\Http\Controllers\SessionsController@destroy')->middleware('auth');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+
+
+//Route::post('sessions', 'App\Http\Controllers\SessionsController@store')->middleware('guest');
+//Route::post('logout', 'App\Http\Controllers\SessionsController@destroy')->middleware('auth');
 
 /*Route::get('categories/{category:slug}', function (Category $category){ 
 
